f5a3163615afd5d0d1a4192fbb0fe8c76e03119e
Kamil Gruszowski <kamil.gruszowski@gmail.com>
2019-06-29 22:25:45 +0200

    get, process and put object to S3 in python exmaple

--- python-example/lambda_function.py
+++ python-example/lambda_function.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import boto3
+from boto3 import client
 from os import environ
 
 def add_watermark(input_image_path, output_image_path, watermark_image_path, position):
@@ -12,21 +12,18 @@ def add_watermark(input_image_path, output_image_path, watermark_image_path, pos
     base_image.save(output_image_path)
 
 def python_add_watermark(event, context):
-
-    print(event)
-    print(context)
-
-    s3 = boto3.client(
+    s3 = client(
         's3',
         aws_access_key_id=environ['public_key'],
         aws_secret_access_key=environ['secret_key']
     )
 
-    bucket_name = 'custom-lambda-php'
-    image_name = 'gotW.jpg'
+    image_name = '/tmp/image.jpg'
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
 
-    s3.download_file(bucket_name, 'origin/gotW.jpg', image_name)
+    s3.download_file(bucket_name, object_key, image_name)
     add_watermark(image_name, image_name, 'stp_logo.png', position=(10, 10))
 
-    s3.upload_file(image_name, bucket_name, "watermark/python_{}".format(image_name))
-
+    key = object_key.split('/')
+    s3.upload_file(image_name, bucket_name, "watermarked/{}".format(key[-1]))
